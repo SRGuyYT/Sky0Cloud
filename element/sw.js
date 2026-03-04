b43134a655d9e6211eceef7ed4442886655d9e2c
SRGuyYT <chapmlinks@gmail.com>
2026-03-03 19:08:28 -0500

    Add files via upload
    Signed-off-by: SRGuyYT <chapmlinks@gmail.com>

---
+++ element/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// DO NOT cache anything (important for debugging + Matrix)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request, {
+      cache: "no-store"
+    }).catch(() => {
+      return new Response("Offline", {
+        status: 503,
+        statusText: "Offline"
+      });
+    })
+  );
+});
